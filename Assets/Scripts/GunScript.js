43fd8aa4040fd144d4faee2ed0bf5b8ebdb90af5
Ryan Felton <quakerassassin@gmail.com>
2014-02-11 22:51:52 -0500

    Added base junk
    There are probably some script errors in PlayerMovement.js in the
    running code, should be fixed shortly

---
+++ Assets/Scripts/GunScript.js
@@ -0,0 +1,23 @@
+﻿var cameraObject : GameObject;
+@HideInInspector
+var targetXRotation : float;
+@HideInInspector
+var targetYRotation : float;
+@HideInInspector
+var targetXRotationV : float;
+@HideInInspector
+var targetYRotationV : float;
+
+var rotateSpeed : float = 0.2;
+
+var holdHeight : float = - 0.5;
+var holdSide : float = 0.5;
+
+function Start () {
+
+}
+
+function Update () 
+{
+	transform.position = cameraObject.transform.position + Quaternion.Euler(0, targetYRotation, 0) * Vector3(holdSide, holdHeight, 0);
+}
---
+++ Assets/Scripts/Inventory.js
@@ -0,0 +1,22 @@
+﻿var song : AudioClip;
+private var coatOpen : boolean = false;
+
+function Update () {
+//Coat Inventrory
+	if (Input.GetKeyDown ("e"))
+		{
+			coatOpen = true;
+		}
+	if (Input.GetKeyUp ("e"))
+		{
+			coatOpen = false;
+		}
+}
+
+function OnGUI () {
+	if (coatOpen == true) {
+		(GUI.Button (Rect (10,10,150,100), "Button")); {
+			print ("You clicked the button!");
+		}
+	}
+}
